5976dc80b3d68b20ed2ad8e447b386afc0d3062c
Stefano Pogliani <stefano@spogliani.net>
2018-03-26 23:50:49 +0100

    OpenTracing configuration options

---
+++ util/tracing/src/config.rs
@@ -0,0 +1,134 @@
+/// Supported tracing backends and their configuration.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Serialize, Deserialize)]
+#[serde(tag = "backend", content = "options", deny_unknown_fields)]
+pub enum Config {
+    /// The `Noop` tracer (default).
+    ///
+    /// A tracer that discards all spans.
+    /// Used when integration with distributed tracing is not needed.
+    #[serde(rename = "noop")]
+    Noop,
+
+    /// [Zipkin] tracer backend.
+    ///
+    /// Spans are sent to [Zipkin] over the [Kafka] collector.
+    ///
+    /// [Kafka]: https://kafka.apache.org/
+    /// [Zipkin]: https://zipkin.io/
+    #[serde(rename = "zipkin")]
+    Zipkin {
+        /// Value for the [Zipkin] service name field.
+        /// 
+        /// [Zipkin]: https://zipkin.io/
+        service_name: String,
+
+        /// List of URLs to seed the [Kafka] client.
+        ///
+        /// [Kafka]: https://kafka.apache.org/
+        kafka: Vec<String>,
+
+        /// [Kafka] topic to publish spans to (defaults to `zipkin`).
+        ///
+        /// [Kafka]: https://kafka.apache.org/
+        #[serde(default = "Config::default_zipkin_topic")]
+        topic: String,
+    },
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config::Noop
+    }
+}
+
+impl Config {
+    fn default_zipkin_topic() -> String { String::from("zipkin") }
+}
+
+
+#[cfg(test)]
+mod tests {
+    mod noop {
+        use serde_yaml;
+        use super::super::Config;
+
+        #[test]
+        fn deserialise() {
+            let text = "backend: noop";
+            let config: Config = serde_yaml::from_str(text).unwrap();
+            assert_eq!(config, Config::Noop);
+        }
+
+        #[test]
+        fn serialise() {
+            let config = Config::Noop;
+            let text = serde_yaml::to_string(&config).unwrap();
+            assert_eq!(text, "---\nbackend: noop");
+        }
+    }
+
+    mod zipkin {
+        use serde_yaml;
+        use super::super::Config;
+
+        #[test]
+        fn deserialise() {
+            let text = r#"backend: zipkin
+options:
+    service_name: abc
+    kafka:
+        - def
+        - ghi
+    topic: test"#;
+            let config: Config = serde_yaml::from_str(text).unwrap();
+            assert_eq!(config, Config::Zipkin {
+                service_name: String::from("abc"),
+                kafka: vec![String::from("def"), String::from("ghi")],
+                topic: String::from("test"),
+            });
+        }
+
+        #[test]
+        fn deserialise_defaults() {
+            let text = r#"backend: zipkin
+options:
+    service_name: abc
+    kafka:
+        - def
+        - ghi"#;
+            let config: Config = serde_yaml::from_str(text).unwrap();
+            assert_eq!(config, Config::Zipkin {
+                service_name: String::from("abc"),
+                kafka: vec![String::from("def"), String::from("ghi")],
+                topic: String::from("zipkin"),
+            });
+        }
+
+        #[test]
+        #[should_panic(expected = "missing field `kafka`")]
+        fn deserialise_fails() {
+            let text = r#"backend: zipkin
+options:
+    service_name: abc"#;
+            let _config: Config = serde_yaml::from_str(text).unwrap();
+        }
+
+        #[test]
+        fn serialise() {
+            let config = Config::Zipkin {
+                service_name: String::from("abc"),
+                kafka: vec![String::from("def"), String::from("ghi")],
+                topic: String::from("test"),
+            };
+            let text = serde_yaml::to_string(&config).unwrap();
+            assert_eq!(text, r#"---
+backend: zipkin
+options:
+  service_name: abc
+  kafka:
+    - def
+    - ghi
+  topic: test"#);
+        }
+    }
+}
--- util/tracing/src/lib.rs
+++ util/tracing/src/lib.rs
@@ -2,3 +2,10 @@ extern crate opentracingrust;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+#[cfg(test)]
+extern crate serde_yaml;
+
+
+mod config;
+
+pub use self::config::Config;
