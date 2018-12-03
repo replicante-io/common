39fe4ca7d4a7919c036dd753b2bd624e39fce7dd
Stefano Pogliani <stefano@spogliani.net>
2018-12-03 18:22:20 +0000

    Format `Fail` information for users and logs.

--- util/failure/src/format.rs
+++ util/failure/src/format.rs
@@ -2,6 +2,49 @@ use failure::Fail;
 
 
 /// Format the given `Fail` for display to the user.
-pub fn format_fail<F: Fail>(_fail: &F) -> String {
-    "TODO".into()
+pub fn format_fail(fail: &dyn Fail) -> String {
+    let mut message = String::new();
+    message.push_str(&format!("Error: {}", fail));
+    for cause in fail.iter_causes() {
+        message.push_str(&format!("\n    Caused by: {}", cause));
+    }
+    let bt = match fail.find_root_cause().backtrace() {
+        None => None,
+        Some(ref bt) if bt.to_string() == "" => None,
+        Some(bt) => Some(bt),
+    };
+    if let Some(bt) = bt {
+        message.push_str(&format!("\n    Backtrace: {}", bt));
+    }
+    message
+}
+
+
+#[cfg(test)]
+mod test {
+    use failure::Fail;
+    use failure::err_msg;
+
+    use super::format_fail;
+
+
+    #[test]
+    fn flat_error() {
+        let error = err_msg("test");
+        let msg = format_fail(error.as_ref());
+        assert_eq!(msg, "Error: test");
+    }
+
+    #[test]
+    fn nested_errors() {
+        let error = err_msg("errors")
+            .context("more")
+            .context("some")
+            .context("test");
+        let msg = format_fail(&error);
+        assert_eq!(msg, r#"Error: test
+    Caused by: some
+    Caused by: more
+    Caused by: errors"#);
+    }
 }
--- util/failure/src/log.rs
+++ util/failure/src/log.rs
@@ -6,16 +6,69 @@ use slog::Serializer;
 
 
 /// Extract failure information to be added to structured logging.
-pub fn failure_info<F: Fail>(_fail: &F) -> FailureInfo {
-    FailureInfo {}
+pub fn failure_info(fail: &dyn Fail) -> FailureInfo {
+    let trace = match Fail::find_root_cause(fail).backtrace() {
+        None => None,
+        Some(ref bt) if bt.to_string() == "" => None,
+        Some(bt) => Some(bt.to_string()),
+    };
+    FailureInfo {
+        cause: fail.cause().map(|cause| cause.find_root_cause().to_string()),
+        layers: Fail::iter_chain(fail).count(),
+        message: fail.to_string(),
+        trace,
+    }
 }
 
 
 /// Container for extracted failure information that implements `slog::KV`.
-pub struct FailureInfo {}
+pub struct FailureInfo {
+    cause: Option<String>,
+    layers: usize,
+    message: String,
+    trace: Option<String>,
+}
 
 impl KV for FailureInfo {
-   fn serialize(&self, _record: &Record, _serializer: &mut Serializer) -> ::slog::Result {
-       Ok(())
+   fn serialize(&self, _record: &Record, serializer: &mut Serializer) -> ::slog::Result {
+       if let Some(cause) = self.cause.as_ref() {
+           serializer.emit_str("error_cause", cause)?;
+       }
+       if let Some(trace) = self.trace.as_ref() {
+           serializer.emit_str("error_trace", trace)?;
+       }
+       serializer.emit_usize("error_layers", self.layers)?;
+       serializer.emit_str("error_message", &self.message)
    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use failure::Fail;
+    use failure::err_msg;
+
+    use super::failure_info;
+
+
+    #[test]
+    fn flat_error() {
+        let error = err_msg("test");
+        let info = failure_info(error.as_ref());
+        assert_eq!(info.cause, None);
+        assert_eq!(info.layers, 1);
+        assert_eq!(info.message, "test");
+    }
+
+    #[test]
+    fn nested_errors() {
+        let error = err_msg("errors")
+            .context("more")
+            .context("some")
+            .context("test");
+        let info = failure_info(&error);
+        assert_eq!(info.cause, Some("errors".into()));
+        assert_eq!(info.layers, 4);
+        assert_eq!(info.message, "test");
+    }
 }
