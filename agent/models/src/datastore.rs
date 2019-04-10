e232a8c58ba02c03adb0a878f826791f6fed7b72
Stefano Pogliani <stefano@spogliani.net>
2019-04-10 21:49:41 +0100

    Update datastore info with cluster id and display name

--- agent/models/src/datastore.rs
+++ agent/models/src/datastore.rs
@@ -1,14 +1,21 @@
 /// Datastore version details.
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Serialize, Deserialize)]
 pub struct DatastoreInfo {
-    pub cluster: String,
+    pub cluster_display_name: Option<String>,
+    pub cluster_id: String,
     pub kind: String,
     pub name: String,
     pub version: String,
 }
 
 impl DatastoreInfo {
-    pub fn new<S1, S2, S3, S4>(cluster: S1, kind: S2, name: S3, version: S4) -> DatastoreInfo
+    pub fn new<S1, S2, S3, S4>(
+        cluster_display_name: Option<String>,
+        cluster_id: S1,
+        kind: S2,
+        name: S3,
+        version: S4,
+    ) -> DatastoreInfo
     where
         S1: Into<String>,
         S2: Into<String>,
@@ -16,7 +23,8 @@ impl DatastoreInfo {
         S4: Into<String>,
     {
         DatastoreInfo {
-            cluster: cluster.into(),
+            cluster_display_name,
+            cluster_id: cluster_id.into(),
             kind: kind.into(),
             name: name.into(),
             version: version.into(),
@@ -32,17 +40,45 @@ mod tests {
 
     #[test]
     fn from_json() {
-        let payload = r#"{"cluster":"cluster","kind":"DB","name":"Name","version":"1.2.3"}"#;
+        let payload = concat!(
+            r#"{"cluster_display_name":null,"cluster_id":"id","kind":"DB","#,
+            r#""name":"Name","version":"1.2.3"}"#
+        );
         let info: DatastoreInfo = serde_json::from_str(payload).unwrap();
-        let expected = DatastoreInfo::new("cluster", "DB", "Name", "1.2.3");
+        let expected = DatastoreInfo::new(None, "id", "DB", "Name", "1.2.3");
+        assert_eq!(info, expected);
+    }
+
+    #[test]
+    fn from_json_with_display_name() {
+        let payload = concat!(
+            r#"{"cluster_display_name":"display name","cluster_id":"id","kind":"DB","#,
+            r#""name":"Name","version":"1.2.3"}"#
+        );
+        let info: DatastoreInfo = serde_json::from_str(payload).unwrap();
+        let expected = DatastoreInfo::new(Some("display name".into()), "id", "DB", "Name", "1.2.3");
         assert_eq!(info, expected);
     }
 
     #[test]
     fn to_json() {
-        let info = DatastoreInfo::new("cluster", "DB", "Name", "1.2.3");
+        let info = DatastoreInfo::new(None, "id", "DB", "Name", "1.2.3");
         let payload = serde_json::to_string(&info).unwrap();
-        let expected = r#"{"cluster":"cluster","kind":"DB","name":"Name","version":"1.2.3"}"#;
+        let expected = concat!(
+            r#"{"cluster_display_name":null,"cluster_id":"id","kind":"DB","#,
+            r#""name":"Name","version":"1.2.3"}"#
+        );
+        assert_eq!(payload, expected);
+    }
+
+    #[test]
+    fn to_json_with_display() {
+        let info = DatastoreInfo::new(Some("display name".into()), "id", "DB", "Name", "1.2.3");
+        let payload = serde_json::to_string(&info).unwrap();
+        let expected = concat!(
+            r#"{"cluster_display_name":"display name","cluster_id":"id","kind":"DB","#,
+            r#""name":"Name","version":"1.2.3"}"#
+        );
         assert_eq!(payload, expected);
     }
 }
