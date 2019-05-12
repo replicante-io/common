a9e2293f308667df70e3e21d573e59923bafc716
Stefano Pogliani <stefano@spogliani.net>
2019-05-12 14:09:04 +0100

    Conver capture_fail into a macro

--- util/failure/src/capture.rs
+++ util/failure/src/capture.rs
@@ -1,8 +1,4 @@
 use failure::Fail;
-use slog::error;
-use slog::Logger;
-
-use super::failure_info;
 
 /// Capture a [`Fail`] and prevent propagation.
 ///
@@ -14,8 +10,45 @@ use super::failure_info;
 ///   * Reported to sentry (if enabled)
 ///   * Logged to the provided logger.
 ///
+/// # Example
+/// ```ignore
+/// use replicante_util_failure::capture_fail;
+/// use replicante_util_failure::failure_info;
+///
+/// capture_fail!(&error, logger, "logging: {}", "test");
+/// capture_fail!(&error, logger, "logging: {}", "test"; "key" => ?"value");
+/// capture_fail!(&error, logger, "logging: {}", "test"; failure_info(&error));
+/// ```
+///
 /// [`Fail`]: https://docs.rs/failure/0.1.5/failure/trait.Fail.html
-pub fn capture_fail(logger: &Logger, error: &dyn Fail) {
+#[macro_export]
+macro_rules! capture_fail(
+    ($error:expr, $($args:tt)+) => {
+        $crate::capture_fail_inner($error);
+        slog::error!($($args)+);
+    }
+);
+
+/// Helper function called from the `capture_fail` macro for extra processing.
+#[doc(hidden)]
+pub fn capture_fail_inner(error: &dyn Fail) {
     sentry::integrations::failure::capture_fail(error);
-    error!(logger, "Capture unhandled failure"; failure_info(error));
+}
+
+#[cfg(test)]
+mod tests {
+    use slog::o;
+    use slog::Discard;
+    use slog::Logger;
+
+    use super::super::failure_info;
+
+    #[test]
+    fn capture_with_log() {
+        let logger = Logger::root(Discard, o!());
+        let error = failure::err_msg("test");
+        capture_fail!(error.as_fail(), logger, "logging: {}", "test");
+        capture_fail!(error.as_fail(), logger, "logging: {}", "test"; failure_info(error.as_fail()));
+        capture_fail!(error.as_fail(), logger, "logging: {}", "test"; "key" => ?"value");
+    }
 }
--- util/failure/src/lib.rs
+++ util/failure/src/lib.rs
@@ -9,7 +9,8 @@ mod capture;
 mod format;
 mod log;
 
-pub use self::capture::capture_fail;
+#[doc(hidden)]
+pub use self::capture::capture_fail_inner;
 pub use self::format::format_fail;
 pub use self::format::SerializableFail;
 pub use self::log::failure_info;
