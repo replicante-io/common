e7ff2dc033e3da3ba6bc35c5abfcf1ed25de9522
Stefano Pogliani <stefano@spogliani.net>
2019-12-17 20:18:08 +0000

    Remove prematurely added action states

--- models/agent/src/actions/enums.rs
+++ models/agent/src/actions/enums.rs
@@ -3,31 +3,29 @@ use serde_derive::Serialize;
 
 /// Entity (system, user, ...) that requested the action to be performed.
 #[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
-#[serde(rename_all = "UPPERCASE")]
 pub enum ActionRequester {
+    /// The action was requested over the API.
+    #[serde(rename = "API")]
     Api,
 }
 
 /// Current state of an action execution.
 #[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
-#[serde(rename_all = "UPPERCASE")]
 pub enum ActionState {
-    /// The action is to be cancelled, but that has not happened yet.
-    Cancel,
-
-    /// The action was successfully cancelled.
-    Cancelled,
-
     /// The action was successfully completed.
+    #[serde(rename = "DONE")]
     Done,
 
     /// The action ended with an error.
+    #[serde(rename = "FAILED")]
     Failed,
 
     /// The action has just been sheduled and is not being executed yet.
+    #[serde(rename = "NEW")]
     New,
 
     /// The action was started by the agent and is in progress.
+    #[serde(rename = "RUNNING")]
     Running,
 }
 
@@ -35,7 +33,6 @@ impl ActionState {
     /// True if the action is finished (failed or succeeded).
     pub fn is_finished(&self) -> bool {
         match self {
-            ActionState::Cancelled => true,
             ActionState::Done => true,
             ActionState::Failed => true,
             _ => false,
