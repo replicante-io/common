c1cc5f683722ee9a7e982b17425bcf864cd362b8
Stefano Pogliani <stefano@spogliani.net>
2020-05-07 22:19:29 +0100

    ActixWeb late `AppConfig` manager

--- util/actixweb/src/lib.rs
+++ util/actixweb/src/lib.rs
@@ -1,5 +1,8 @@
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::Arc;
+
+use actix_web::web::ServiceConfig;
 
 mod descriptor;
 mod error;
@@ -24,3 +27,33 @@ pub use self::tracing::TracingMiddleware;
 
 /// Type alias for futures returned by middleweres to keep clippy happy.
 type BoxedFuture<R, E> = Pin<Box<dyn Future<Output = std::result::Result<R, E>>>>;
+
+/// On-demand Actix Web `App` configuration with functions and closures.
+#[derive(Clone)]
+pub struct AppConfig<T> {
+    configs: Vec<Arc<dyn Fn(&mut ServiceConfig, &T) + Send + Sync>>,
+}
+
+impl<T> AppConfig<T> {
+    /// Run all the register handles to configure the given app.
+    pub fn configure(&self, app: &mut ServiceConfig, context: &T) {
+        for config in &self.configs {
+            config(app, context);
+        }
+    }
+
+    /// Register an app configuration function to be run later.
+    pub fn register<F>(&mut self, config: F)
+    where
+        F: Fn(&mut ServiceConfig, &T) + 'static + Send + Sync,
+    {
+        self.configs.push(Arc::new(config));
+    }
+}
+
+impl<T> Default for AppConfig<T> {
+    fn default() -> Self {
+        let configs = Vec::new();
+        AppConfig { configs }
+    }
+}
